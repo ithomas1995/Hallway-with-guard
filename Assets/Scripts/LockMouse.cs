a6788a79a2f317a0274095a51251bd04c0b7a1e5
roolies <rilie.s@comcast.net>
2022-02-20 18:41:49 -0500

    fixedstuff

---
+++ Assets/Scripts/LockMouse.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockMouse : MonoBehaviour
+{
+     
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
